9484cba02c83c769dd2db6be57d177a56500bf91
Eller Jones Silveira <eller@simetria.studio>
2021-09-22 15:45:39 -0300

    .

--- routes/routes.php
+++ routes/routes.php
@@ -39,6 +39,26 @@ Route::prefix('registration')->group(function () {
 		Route::put('services/registration', [App\Http\Controllers\Registration\Stock\ProductController::class, 'update_service'])->name('registration.stock.services');
 		Route::delete('services/registration', [App\Http\Controllers\Registration\Stock\ProductController::class, 'destroy_service'])->name('registration.stock.services');
 
+		Route::get('product_gf/registration', [App\Http\Controllers\Registration\Stock\ProductGFController::class, 'view_product_group_family'])->name('registration.stock.product_gf');
+		Route::post('product_gf/registration', [App\Http\Controllers\Registration\Stock\ProductGFController::class, 'store_product_group_family'])->name('registration.stock.product_gf');
+		Route::put('product_gf/registration', [App\Http\Controllers\Registration\Stock\ProductGFController::class, 'update_product_group_family'])->name('registration.stock.product_gf');
+		Route::delete('product_gf/registration', [App\Http\Controllers\Registration\Stock\ProductGFController::class, 'destroy_product_group_family'])->name('registration.stock.product_gf');
+
+		Route::get('manufacturer/registration', [App\Http\Controllers\Registration\Stock\ManufacturerController::class, 'view_manufacturer'])->name('registration.stock.manufacturer');
+		Route::post('manufacturer/registration', [App\Http\Controllers\Registration\Stock\ManufacturerController::class, 'store_manufacturer'])->name('registration.stock.manufacturer');
+		Route::put('manufacturer/registration', [App\Http\Controllers\Registration\Stock\ManufacturerController::class, 'update_manufacturer'])->name('registration.stock.manufacturer');
+		Route::delete('manufacturer/registration', [App\Http\Controllers\Registration\Stock\ManufacturerController::class, 'destroy_manufacturer'])->name('registration.stock.manufacturer');
+
+		Route::get('deposited/registration', [App\Http\Controllers\Registration\Stock\DepositedController::class, 'view_deposited'])->name('registration.stock.deposited');
+		Route::post('deposited/registration', [App\Http\Controllers\Registration\Stock\DepositedController::class, 'store_deposited'])->name('registration.stock.deposited');
+		Route::put('deposited/registration', [App\Http\Controllers\Registration\Stock\DepositedController::class, 'update_deposited'])->name('registration.stock.deposited');
+		Route::delete('deposited/registration', [App\Http\Controllers\Registration\Stock\DepositedController::class, 'destroy_deposited'])->name('registration.stock.deposited');
+
+		Route::get('type_sale/registration', [App\Http\Controllers\Registration\Stock\TypeSaleController::class, 'view_type_sale'])->name('registration.stock.type_sale');
+		Route::post('type_sale/registration', [App\Http\Controllers\Registration\Stock\TypeSaleController::class, 'store_type_sale'])->name('registration.stock.type_sale');
+		Route::put('type_sale/registration', [App\Http\Controllers\Registration\Stock\TypeSaleController::class, 'update_type_sale'])->name('registration.stock.type_sale');
+		Route::delete('type_sale/registration', [App\Http\Controllers\Registration\Stock\TypeSaleController::class, 'destroy_type_sale'])->name('registration.stock.type_sale');
+
 	});
 
 });
@@ -46,7 +66,7 @@ Route::prefix('registration')->group(function () {
 Route::prefix('commercial')->group(function () {
 });
 
-Route::prefix('inventory')->group(function () {
+Route::prefix('stock')->group(function () {
 });
 
 Route::prefix('finance')->group(function () {
